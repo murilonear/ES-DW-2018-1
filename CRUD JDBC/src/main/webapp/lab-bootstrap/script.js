a9c17fcd84b21a4dba364f7d31b857b0bc8529a2
Murilo Eduardo Alves Vaz <murilo.near@gmail.com>
2018-06-25 11:48:10 -0300

    Atividade "CRUD JDBC"

--- CRUD JDBC/src/main/webapp/lab-bootstrap/script.js
+++
@@ -1,9 +0,0 @@
-function maiorMenor() {
-    var idadeInput = document.getElementById("idade");
-    var idade = idadeInput.value;
-    if (idade >= 18) {
-        alert("Maior de idade!");
-    } else {
-        alert("Menor de idade!");
-    }
-}
